3944a24e7a15839020eb0bea9f3def17d872a2eb
adrianmeler <80096433+adrianmeler@users.noreply.github.com>
2024-01-01 12:13:51 +0100

    Add files via upload

---
+++ empleados.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 01, 2024 at 12:12 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `empleados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `ID` int(11) NOT NULL,
+  `APELLIDO` varchar(255) DEFAULT NULL,
+  `CARGO` varchar(255) DEFAULT NULL,
+  `FECHA` date DEFAULT NULL,
+  `NOMBRE` varchar(255) DEFAULT NULL,
+  `SALARIO` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `empleado`
+--
+
+INSERT INTO `empleado` (`ID`, `APELLIDO`, `CARGO`, `FECHA`, `NOMBRE`, `SALARIO`) VALUES
+(2, 'Meler', 'Jefe', '2023-04-21', 'Adrian', 4000.9),
+(3, 'Meler', 'Asistente', '2023-04-30', 'Alan', 2000.5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
